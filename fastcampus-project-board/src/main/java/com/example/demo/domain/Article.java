e03fe391f829e7f5f4f576cdbe1516c8019f39cb
sbhan <sbhan@suresofttech.com>
2025-09-05 17:21:21 +0900

    feat: 테스트 환경 구성 중
    #11

--- fastcampus-project-board/src/main/java/com/example/demo/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/example/demo/domain/Article.java
@@ -1,22 +1,46 @@
 package com.example.demo.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Getter
+@Table(name = "article")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "content")
     private String content;
 
+    @Column(name = "hashtag")
     private String hashtag;
 
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @Column(name = "created_by")
     private String createdBy;
 
+    @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
 
+    @Column(name = "modified_by")
     private String modifiedBy;
+
+    public Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
 }
